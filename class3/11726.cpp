eba66207d8c357fec75d77fa9731d2fc840dcb7f
park-jaeo <pg01198@naver.com>
2025-07-29 16:47:23 +0900

    [feat] : 11726번 풀이

---
+++ class3/11726.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int N;
+  cin >> N;
+  vector<int> dp;
+  dp.push_back(0); // 1-index
+  dp.push_back(1); // dp[1]
+  dp.push_back(2); // dp[2]
+
+  for(int i = 3; i < 1000; ++i){
+    dp.push_back(dp[i - 1] + dp[i - 2]);
+  }
+  cout << dp[N] << endl;
+  return 0;
+}
